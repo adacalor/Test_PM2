ad05345f6b58ff93f6a22f518d3bf4445d0d302f
[adacalor] <[adacalor@gmail.com]>
2020-08-18 11:37:58 +0200

    ed

---
+++ test_pm2.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 18 11:35:32 2020
+
+@author: Collabcap
+"""
+
+import time 
+
+
+
+
+print( "start test")
+
+for i in range(100) :
+    time.sleep(1)
+    print(i)
+
+print(" stop test ")
+
+time.sleep(5)
+
+
+
+
+
+
+
+
+
